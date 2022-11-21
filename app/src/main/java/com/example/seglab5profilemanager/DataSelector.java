8e4386b260d9e8512cc13c415eaca382d703caf3
NJValin <neilvalin1@gmail.com>
2022-11-20 19:43:35 -0500

    Finished

--- app/src/main/java/com/example/seglab5profilemanager/DataSelector.java
+++ app/src/main/java/com/example/seglab5profilemanager/DataSelector.java
@@ -9,25 +9,123 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
 @SuppressLint("MissingInflatedId")
 public class DataSelector extends AppCompatActivity {
     EditText sportClub;
-    Button submitBtn;
+    ImageButton submitBtn, korea, canada, egypt, france, japan, spain, turkey, uk, us;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_selector);
 
-        sportClub = findViewById(R.id.sportClub);
-        submitBtn = findViewById((R.id.submitBtn));
 
-        submitBtn.setOnClickListener(new View.OnClickListener() {
+        korea = (ImageButton) findViewById(R.id.korea);
+        canada = (ImageButton) findViewById(R.id.canada);
+        egypt = (ImageButton) findViewById(R.id.egypt);
+        france = (ImageButton) findViewById(R.id.france);
+        japan = (ImageButton) findViewById(R.id.japan);
+        spain = (ImageButton) findViewById(R.id.spain);
+        turkey = (ImageButton) findViewById(R.id.turkey);
+        uk = (ImageButton) findViewById(R.id.britain);
+        us = (ImageButton) findViewById(R.id.america);
+
+        korea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent returnIntent = new Intent();
 
-                returnIntent.putExtra("text_data", sportClub.getText().toString());
+                returnIntent.putExtra("image", "korea");
+                setResult(RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+        canada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("image", "canada");
+                setResult(RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+        egypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("image", "egypt");
+                setResult(RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+        france.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("image", "france");
+                setResult(RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+        japan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("image", "japan");
+                setResult(RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+        spain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("image", "spain");
+                setResult(RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+        turkey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("image", "turkey");
+                setResult(RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+        uk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("image", "uk");
+                setResult(RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+        us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = new Intent();
+
+                returnIntent.putExtra("image", "us");
                 setResult(RESULT_OK, returnIntent);
 
                 finish();
--- app/src/main/java/com/example/seglab5profilemanager/MainActivity.java
+++ app/src/main/java/com/example/seglab5profilemanager/MainActivity.java
@@ -5,30 +5,46 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    Button mapBtn;
-    TextView displayText;
+    Button mapBtn, setValue;
+    ImageButton flag;
+    EditText teamname, teamaddress;
+    TextView nameDisplay, addressDisplay;
     ActivityResultLauncher<Intent> launcher;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapBtn = (Button) findViewById(R.id.mapsBtn);
+        setValue = (Button) findViewById(R.id.setValues);
+        flag = (ImageButton) findViewById(R.id.flagBtn);
+        teamname = (EditText) findViewById(R.id.teamNameBox);
+        teamaddress = (EditText) findViewById(R.id.postalCodebox);
+        nameDisplay = (TextView) findViewById(R.id.teamnamedisplay);
+        addressDisplay = (TextView) findViewById(R.id.addressdisplay);
 
-
-        mapBtn = findViewById(R.id.mapsBtn);
-        displayText = findViewById(R.id.displayTxt);
         launcher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>(){
@@ -36,8 +52,38 @@ public class MainActivity extends AppCompatActivity {
                     public void onActivityResult(ActivityResult result){
                         if(result.getResultCode()== Activity.RESULT_OK) {
                             Intent data = result.getData();
-                            String textData = data.getStringExtra("text_data");
-                            displayText.setText(textData);
+                            //Bundle data = result.getData().getExtras();
+                            String flagName = data.getStringExtra("image");
+                            if (flagName.equals("korea")) {
+                                flag.setImageResource(R.drawable.flag_kr);
+                            }
+                            else if (flagName.equals("canada")) {
+
+                                flag.setImageResource(R.drawable.flag_ca);
+                            }
+                            else if (flagName.equals("egypt")) {
+
+                                flag.setImageResource(R.drawable.flag_eg);
+                            }
+                            else if (flagName.equals("france")) {
+                                flag.setImageResource(R.drawable.flag_fr);
+                            }
+                            else if (flagName.equals("japan")) {
+                                flag.setImageResource(R.drawable.flag_jp);
+                            }
+                            else if (flagName.equals("spain")) {
+                                flag.setImageResource(R.drawable.flag_sp);
+                            }
+                            else if (flagName.equals("turkey")) {
+                                flag.setImageResource(R.drawable.flag_tr);
+                            }
+                            else if (flagName.equals("uk")) {
+                                flag.setImageResource(R.drawable.flag_uk);
+                            }
+                            else if (flagName.equals("us")) {
+                                flag.setImageResource(R.drawable.flag_us);
+                            }
+
                         }
                     }
                 }
@@ -45,10 +91,48 @@ public class MainActivity extends AppCompatActivity {
         mapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                OnOpenInGoogleMaps(view);
+            }
+        });
+        flag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), DataSelector.class);
                 launcher.launch(intent);
             }
         });
+        setValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (teamaddress.getText().toString().equals("")&& teamname.getText().toString().equals("")) {
+                    return;
+                }
+                else if (teamaddress.getText().toString().equals("")) {
+                    nameDisplay.setText(teamname.getText().toString());
+                    teamaddress.setText("");
+                    teamname.setText("");
+                }
+                else if (teamname.getText().toString().equals(""))  {
+                    addressDisplay.setText(teamaddress.getText().toString());
+                    teamaddress.setText("");
+                    teamname.setText("");
+                }
+                else {
+                    nameDisplay.setText(teamname.getText().toString());
+                    addressDisplay.setText(teamaddress.getText().toString());
+                    teamaddress.setText("");
+                    teamname.setText("");
+                }
+            }
+        });
     }
+    public void OnOpenInGoogleMaps(View view) {
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+addressDisplay.getText().toString());
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+
 
 }
